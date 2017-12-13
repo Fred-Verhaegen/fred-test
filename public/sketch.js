b380b973808dfb8992482a0d0272db14921ffd09
Fred Verhaegen <fredverhaegen@oBox-5BDD.local>
2017-12-13 17:22:42 +0100

    first upload

--- public/sketch.js
+++ public/sketch.js
@@ -10,7 +10,7 @@ function setup() {
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('https://fredgame.herokuapp.com');
+  socket = io.connect('http://valuer-cameras-88474.bitballoon.com/');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
